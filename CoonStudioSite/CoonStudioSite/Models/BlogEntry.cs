8d8c9039291c5f4c1a28e9d90a5a51c38bb63130
Dmitry <dimon13142@mail.ru>
2018-02-03 16:47:27 +0300

    Add Required Attribute

--- CoonStudioSite/CoonStudioSite/Models/BlogEntry.cs
+++ CoonStudioSite/CoonStudioSite/Models/BlogEntry.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoonGamesSite.Models
 {
     public class BlogEntry
     {
         public int ID { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
         public DateTime Date { get; set; }
         public string Content { get; set; }
         public string Tag { get; set; }
